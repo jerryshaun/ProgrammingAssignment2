4254278e9520f25fe2889c975746885ad0249fbf
jerryshaun <jerryshaun@hotmail.com>
2015-04-22 07:44:44 -0500

    Early notes and psuedocode

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## x should be a square matrix.
+    
+    ## Steps to solve:
+    ##  1. Find inverse, and return
+    
+    
+    
+    
+    
 }
 
 
@@ -12,4 +20,26 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    ## Steps to solve:
+    ##  1. Get the inverse from cache
+    ##  2. See if that inverse is null.
+    ##      3. If not, pull value from cache
+    ##      4. If so, do calculation
+    ##  5. Return inverse
+    
+    
+    ## inv will store inverse of matrix
+    inv <- ##Get the Inverse
+    
+    ## Check if inverse already set, and if so return it.
+    if (!is.null(inv)) {
+        message("Getting inverse from cache")
+        return(inv)
+    }
+    
+    ## Otherwise not set - use make function to set
+
+    
+    
 }
