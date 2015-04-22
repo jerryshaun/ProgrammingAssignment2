2c3a5bcdef7afcdf4acc2deb08551b1f6d741ffa
jerryshaun <jerryshaun@hotmail.com>
2015-04-22 13:10:22 -0500

    First working version

--- cachematrix.R
+++ cachematrix.R
@@ -6,40 +6,73 @@
 makeCacheMatrix <- function(x = matrix()) {
     ## x should be a square matrix.
     
-    ## Steps to solve:
-    ##  1. Find inverse, and return
-    
-    
-    
-    
-    
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
+
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    
-    ## Steps to solve:
-    ##  1. Get the inverse from cache
-    ##  2. See if that inverse is null.
-    ##      3. If not, pull value from cache
-    ##      4. If so, do calculation
-    ##  5. Return inverse
-    
-    
+cacheSolve <- function(x) {
+    ## Return a matrix that is the inverse of 'x'
+
     ## inv will store inverse of matrix
-    inv <- ##Get the Inverse
-    
+    inv <- x$getinverse()           ##Get the Inverse
+
     ## Check if inverse already set, and if so return it.
     if (!is.null(inv)) {
-        message("Getting inverse from cache")
+        message("getting cached data")
         return(inv)
     }
-    
-    ## Otherwise not set - use make function to set
 
+    ## Otherwise not set - use make function to set
+    data <- x$get()
+    inv <- solve(data)
     
+    x$setinv(inv)
+    
+    return(inv)
+
+}
+
+
+test = function(mat){
+    ## @mat: an invertible matrix
+    
+    temp = makeCacheMatrix(mat)
+
+    start.time = Sys.time()
+    cacheSolve(temp)
+    dur = Sys.time() - start.time
+    print(dur)
+    
+    #start.time = Sys.time()
+    #cacheSolve(temp)
+    #dur = Sys.time() - start.time
+    #print(dur)
+}
+
+
+makestuff = function() {
+    
+    #set.seed(1110201)
+    #r = rnorm(1000000)
+    #matl=matrix(r, nrow=1000, ncol=1000)
+    
+    x = rbind(c(1, -1/4), c(-1/4,1))
+    m = makeCacheMatrix(x)
+    m$get()
+    
+
+    test(matl)
     
 }
